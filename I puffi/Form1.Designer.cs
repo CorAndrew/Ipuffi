b36967ad0942f3debbea676bceaa64aa7b983515
AndrewCor <14529982@itisrossi.vi.it>
2026-03-01 11:39:43 +0100

    Aggiungere i file di progetto.

---
+++ I puffi/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace I_puffi
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            Background = new Panel();
+            Casa = new Panel();
+            panel1 = new Panel();
+            puffo1 = new Panel();
+            inizia = new Button();
+            Punteggio = new Label();
+            Background.SuspendLayout();
+            SuspendLayout();
+            // 
+            // Background
+            // 
+            Background.BackColor = Color.FromArgb(128, 255, 128);
+            Background.Controls.Add(Casa);
+            Background.Controls.Add(panel1);
+            Background.Controls.Add(puffo1);
+            Background.Location = new Point(12, 22);
+            Background.Name = "Background";
+            Background.Size = new Size(1551, 696);
+            Background.TabIndex = 0;
+            Background.Visible = false;
+            // 
+            // Casa
+            // 
+            Casa.BackColor = Color.Red;
+            Casa.Location = new Point(1118, 222);
+            Casa.Name = "Casa";
+            Casa.Size = new Size(63, 63);
+            Casa.TabIndex = 2;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.MenuHighlight;
+            panel1.Location = new Point(188, 117);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(79, 163);
+            panel1.TabIndex = 1;
+            // 
+            // puffo1
+            // 
+            puffo1.BackColor = SystemColors.MenuHighlight;
+            puffo1.BackgroundImage = (Image)resources.GetObject("puffo1.BackgroundImage");
+            puffo1.Location = new Point(446, 117);
+            puffo1.Name = "puffo1";
+            puffo1.Size = new Size(156, 233);
+            puffo1.TabIndex = 0;
+            // 
+            // inizia
+            // 
+            inizia.Location = new Point(771, 0);
+            inizia.Name = "inizia";
+            inizia.Size = new Size(75, 23);
+            inizia.TabIndex = 1;
+            inizia.Text = "inizia";
+            inizia.UseVisualStyleBackColor = true;
+            inizia.Click += inizia_Click;
+            // 
+            // Punteggio
+            // 
+            Punteggio.AutoSize = true;
+            Punteggio.Location = new Point(1151, 4);
+            Punteggio.Name = "Punteggio";
+            Punteggio.Size = new Size(97, 15);
+            Punteggio.TabIndex = 2;
+            Punteggio.Text = "Punteggio puffo:";
+            Punteggio.Visible = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1575, 749);
+            Controls.Add(Punteggio);
+            Controls.Add(inizia);
+            Controls.Add(Background);
+            KeyPreview = true;
+            Name = "Form1";
+            Text = "Form1";
+            Background.ResumeLayout(false);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Panel Background;
+        private Panel puffo1;
+        private Button inizia;
+        private Panel panel1;
+        private Label Punteggio;
+        private Panel Casa;
+    }
+}
---
+++ I puffi/Form1.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace I_puffi
+{
+    public partial class Form1 : Form
+    {
+        int velocita = 10;
+        int punti = 0;
+        Random rnd = new Random();
+
+        public struct punto
+        {
+            public int X;
+            public int Y;
+            public punto(int x, int y) { X = x; Y = y; }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.KeyPreview = true;
+            this.KeyDown += new KeyEventHandler(Form1_KeyDown);
+            PosizionaCasaCasuale();
+        }
+
+        private void PosizionaCasaCasuale()
+        {
+            int maxX = this.ClientSize.Width - Casa.Width;
+            int maxY = this.ClientSize.Height - Casa.Height;
+            Casa.Location = new Point(rnd.Next(0, maxX), rnd.Next(0, maxY));
+        }
+
+        private void ControllaCollisione()
+        {
+            if (panel1.Bounds.IntersectsWith(Casa.Bounds))
+            {
+                Punteggio.Visible = true;
+                punti++;
+                Punteggio.Text = "Punteggio puffo: " + punti;
+                PosizionaCasaCasuale();
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                    if (panel1.Top > 0) panel1.Top -= velocita;
+                    break;
+                case Keys.S:
+                    if (panel1.Bottom < this.ClientSize.Height) panel1.Top += velocita;
+                    break;
+                case Keys.A:
+                    if (panel1.Left > 0) panel1.Left -= velocita;
+                    break;
+                case Keys.D:
+                    if (panel1.Right < this.ClientSize.Width) panel1.Left += velocita;
+                    break;
+            }
+            ControllaCollisione();
+        }
+
+        private void inizia_Click(object sender, EventArgs e)
+        {
+            Background.Visible = true;
+            inizia.Visible = false;
+            this.Focus();
+        }
+    }
+}
